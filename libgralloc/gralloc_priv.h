77912283c41ce25c0a3aa6cc6890d2277eb63704
Konsta <konsta09@gmail.com>
2014-05-24 12:55:20 +0300

    display-caf-new: conditionally revert "gralloc: Fix compatibility issue"
    Set COMMON_GLOBAL_CFLAGS += -DNO_COMPAT_GRALLOC_PERFORM
    in BoardConfig.mk to use.
    
    This conditionally reverts commit 80c0c5b5976e08c2e41b38cafba290121ed6246c.
    
    Change-Id: I14294ff5e7b496165903fe364527bc8767a112ae

--- libgralloc/gralloc_priv.h
+++ libgralloc/gralloc_priv.h
@@ -30,7 +30,9 @@
 
 #include <cutils/log.h>
 
+#ifndef NO_COMPAT_GRALLOC_PERFORM
 #define COMPAT_GRALLOC_PERFORM
+#endif
 
 #define ROUND_UP_PAGESIZE(x) ( (((unsigned long)(x)) + PAGE_SIZE-1)  & \
                                (~(PAGE_SIZE-1)) )
